415d2828da4de54beda29107181226a73e676db0
shuhaibXyz <muhammadshuhaibbinabdulaziz@gmail.com>
2024-11-30 00:13:41 +0800

    home.php

---
+++ home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home php</title>
+
+    <!--tautan css swiper-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+
+
+  <!--font tautan cdn yang mengagumkan-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" 
+    integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+     <!--tautan file css khusus-->                
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!--bagian header dimulai-->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+   
+   <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">package</a>
+    <a href="book.php">book</a>
+   </nav>
+
+</section>
+
+
+<!--bagian header berakhir-->
+  
+
+
+
+
+
+
+
+<!--tautan css swiper-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!--tautan file js khusus-->
+<script src="js/script.js"></script>
+
+   
+    
+</body>
+</html>
